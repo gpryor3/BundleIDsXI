bd4743c9c7c90e294d390d737ef73efab728d28a
TD <tdhadvocate@gmail.com>
2018-07-23 17:22:24 -0400

    Removing old files due to having to be renamed and
     completely recompiled in many cases.

--- RootViewController.h
+++
@@ -1,6 +0,0 @@
-@interface RootViewController: UIViewController <UITableViewDelegate, UITableViewDataSource> {
-	UITableView* tabView;
-	NSMutableArray* appNames;
-	NSMutableDictionary* theApps;
-}
-@end
